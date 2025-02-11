d44f921db0c7d03d8a9744606adee797e4c56779
kake <36560265+kake-t@users.noreply.github.com>
2025-02-11 22:26:03 +0900

    Todoのドメイン層を作成

---
+++ src/domain/repositories/TodoRepository.ts
@@ -0,0 +1,9 @@
+import { Todo } from '../entities/Todo';
+
+export interface TodoRepository {
+  getAll(): Promise<Todo[]>;
+  getById(id: string): Promise<Todo | null>;
+  create(todo: Omit<Todo, 'id'>): Promise<Todo>;
+  update(todo: Todo): Promise<Todo>;
+  delete(id: string): Promise<void>;
+}
